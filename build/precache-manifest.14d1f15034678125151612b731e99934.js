c0a733d50941c7338212a07ebe8b0b15f060a09c
NotVinay <vinaypatel_97@yahoo.in>
2019-11-08 23:19:10 +0000

    Added gh pages

---
+++ build/precache-manifest.14d1f15034678125151612b731e99934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e902dc27bedbc5f2c5bfe25730c23e",
+    "url": "/open-ex/index.html"
+  },
+  {
+    "revision": "9f093420a4a2428d6a95",
+    "url": "/open-ex/static/css/main.5f2ad4bc.chunk.css"
+  },
+  {
+    "revision": "7853cb4ca601de54833e",
+    "url": "/open-ex/static/js/2.817b5add.chunk.js"
+  },
+  {
+    "revision": "9f093420a4a2428d6a95",
+    "url": "/open-ex/static/js/main.70a856d3.chunk.js"
+  },
+  {
+    "revision": "9a9f402135da1f34cbb1",
+    "url": "/open-ex/static/js/runtime-main.9516ed25.js"
+  },
+  {
+    "revision": "60ec424535505f6a683dba4d17d17e74",
+    "url": "/open-ex/static/media/logo.60ec4245.svg"
+  }
+]);

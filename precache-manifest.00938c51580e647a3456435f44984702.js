b73c30d497e9a3968717c601b3d4f5ec72909ad5
NotVinay <vinaypatel_97@yahoo.in>
2019-11-08 23:33:05 +0000

    Updates

---
+++ precache-manifest.00938c51580e647a3456435f44984702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cecd6b8837951d9864bc188e9a5eb3e",
+    "url": "/openex/index.html"
+  },
+  {
+    "revision": "6974ae4908319fcf483e",
+    "url": "/openex/static/css/main.5f2ad4bc.chunk.css"
+  },
+  {
+    "revision": "7853cb4ca601de54833e",
+    "url": "/openex/static/js/2.817b5add.chunk.js"
+  },
+  {
+    "revision": "6974ae4908319fcf483e",
+    "url": "/openex/static/js/main.ddfd8835.chunk.js"
+  },
+  {
+    "revision": "ddae04cbad219ecd5d52",
+    "url": "/openex/static/js/runtime-main.97be4519.js"
+  },
+  {
+    "revision": "60ec424535505f6a683dba4d17d17e74",
+    "url": "/openex/static/media/logo.60ec4245.svg"
+  }
+]);

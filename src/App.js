a6486e704b2d849b8440f1c4c40842bcc8ddc616
NotVinay <vinaypatel_97@yahoo.in>
2019-11-19 19:21:54 +0000

    [+][Modified][Added a base url]

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ExchangeRate from "./containers/ExchangeRate/ExchangeRate";
-import classes from './App.module.css';
+import classes from "./App.module.css";
 import Home from "./containers/Home/Home";
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename="/openex">
         <div className={classes.App}>
           <Layout>
             <Route path="/" exact component={Home} />
